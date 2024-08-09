70e2188d17405196ac6a80480ea2dc13a6b70b30
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2024-08-09 19:43:59 +0300

    wETH Balance added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,11 @@
 export const ETH_MAINNET_CHAIN_ID = '0x1';
 export const SEPOLIA_CHAIN_ID = '0xaa36a7';
 export const ALLOWED_NETWORKS = ["0x1", "0xaa36a7"] //allowed networks 
+export const NEXO_TOKEN_CONTRACT_ADDRESS = '0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206';
+export const NEXO_TOKEN_ABI = [
+  "function balanceOf(address owner) view returns (uint256)"
+];
+export const wETH_TOKEN_CONTRACT_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+export const wETH_TOKEN_ABI = [
+    "function balanceOf(address owner) view returns (uint256)"
+  ];
--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -1,33 +1,58 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import { NEXO_TOKEN_CONTRACT_ADDRESS, NEXO_TOKEN_ABI, wETH_TOKEN_CONTRACT_ADDRESS, wETH_TOKEN_ABI } from "../constants/constants.js";
+
 const useBalance = (address) => {
     
-  const [balance, setBalance] = useState(null);
-  const [error, setError] = useState(null);
+    const [ethBalance, setEthBalance] = useState(null);
+    const [nexoBalance, setNexoBalance] = useState(null);
+    const [wethBalance, setWethBalance] = useState(null);
+    const [error, setError] = useState(null);
 
   useEffect(() => {
+
     if (address) {
-      const fetchBalance = async () => {
-        try {
-            // Fetch balance for the given address
-          const balance = await window.ethereum.request({
-            method: 'eth_getBalance',
-            params: [address, 'latest']
-          });
-          // Update state with the new balance
-          setBalance(ethers.formatEther(balance));
-        } catch (err) {
-            //error handling
-          setError('Error fetching balance');
-          console.error(err);
-        }
-      };
+        const fetchBalances = async () => {
+          // Fetch ETH balance
+          try {
+            const ethBalance = await window.ethereum.request({
+              method: 'eth_getBalance',
+              params: [address, 'latest']
+            });
+            setEthBalance(ethers.formatEther(ethBalance));
+          } catch (err) {
+            setError('Error fetching ETH token balance');
+            console.log('ETH Balance Error:', err);
+          }
+          // Fetch NEXO token balance
+          try {
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const contract = new ethers.Contract(NEXO_TOKEN_CONTRACT_ADDRESS, NEXO_TOKEN_ABI, provider);
+            const tokenBalance = await contract.balanceOf(address);
+            setNexoBalance(ethers.formatEther(tokenBalance));
+          } catch (err) {
+            setError('Error fetching NEXO token balance');
+            console.log('NEXO Token Balance Error:', err);
+          }
 
-      fetchBalance();
-    }
-  }, [address]);
+          // Fetch wETH token balance
+          try {
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const contract = new ethers.Contract(wETH_TOKEN_CONTRACT_ADDRESS, wETH_TOKEN_ABI, provider);
+            const tokenBalance = await contract.balanceOf(address);
+            setWethBalance(ethers.formatEther(tokenBalance));
+            
+          } catch (err) {
+            setError('Error fetching wETH token balance');
+            console.log('wETH Token Balance Error:', err);
+          }
+        };
+  
+        fetchBalances();
+      }
+    }, [address]);
 
-  return { balance, error };
+  return { ethBalance, nexoBalance, wethBalance, error };
 };
 
 export default useBalance;
